9f454a39c43c1d82b04ea990dbf3086eec6ad09a
KeylorAGS <keilor.vargas.segura@est.una.ac.cr>
2025-10-30 18:20:13 -0600

    .

--- Backend/src/main/java/hospital/Application.java
+++ Backend/src/main/java/hospital/Application.java
@@ -1,6 +1,5 @@
 package hospital;
 
-
 import hospital.logic.Server;
 
 public class Application {
@@ -8,5 +7,9 @@ public class Application {
     public static void main(String[] args) {
         Server server = new Server();
         server.run();
+        //Nombre:                      Cedula:
+        //Dave Navarro Madrigal        118850610
+        //Keneth Andrey Jara Herrera   402600458
+        //Keilor Andrey Vargas Segura  402670493
     }
 }
--- Fronted/src/main/java/hospital/Main.java
+++ Fronted/src/main/java/hospital/Main.java
@@ -5,5 +5,9 @@ import hospital.presentation.Interfaces.InterfazLogin;
 public class Main {
     public static void main(String[] args) {
         InterfazLogin.ventanaLogin();
+        //Nombre:                      Cedula:
+        //Dave Navarro Madrigal        118850610
+        //Keneth Andrey Jara Herrera   402600458
+        //Keilor Andrey Vargas Segura  402670493
     }
 }
